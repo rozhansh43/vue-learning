18bee45c98a6e2ffb041376d50189520a0fed26d
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-04 22:39:36 -0700

    image hover added

--- pro2/main.js
+++ pro2/main.js
@@ -9,13 +9,26 @@ var app = new Vue({
         variants : [
             {
                 variantId: 234,
-                variantColor: 'Red'
+                variantColor: 'Red',
+                variantImage : 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/8d69fcc04fca40aea88c7eaee3b7c367.jpg'
             },
             {
                 variantId: 344,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage : 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/b44111bc02b641619733c46685033288.jpg'
+
             }
         ],
+        sizes :[ 'medium' , 'large'],
+        cart : 0,
         image : 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/df0823628fca40419cca88368e00955f.jpg'
+    },
+    methods:{
+        cartCounter : function(){
+            this.cart += 1 
+        },
+        updateImage : function(variantImage){
+            this.image = variantImage
+        }
     }
 })
