2963134f007150b5dac1134a5b22440e9c444e91
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-05 00:25:31 -0700

    template added

--- pro2/main.js
+++ pro2/main.js
@@ -1,32 +1,93 @@
-var app = new Vue({
-    el:'#app',
-    data:{
-        name :'sea food',
-        brand : 'ba',
-        titleLink : 'https://google.com',
-        details : [ 'fish' , 'ocean' , 'fresh'],
-        variants : [
-            {
-                variantId: 234,
-                variantColor: 'Red',
-                variantQuantity : 0,
-                isOnSale : true ,
-                variantImage : 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/8d69fcc04fca40aea88c7eaee3b7c367.jpg'
-            },
-            {
-                variantId: 344,
-                variantColor: 'blue',
-                variantQuantity : 10 ,
-                isOnSale : false ,
-                variantImage : 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/b44111bc02b641619733c46685033288.jpg'
+Vue.component('product',{
+    template:`
+        <div class="product">
+            <div class="product-img">
+                <img v-bind:src="image" />
+                <span v-if="isOnSale">
+                    onsale
+                </span>
+            </div>
 
-            }
-        ],
-        sizes :[ 'medium' , 'large'],
-        white : 'white',
-        width: 50,
-        cart : 0,
-        selectedVariant :0
+            <div class="product-info">
+                <h2 v-bind:href="titleLink">
+                    {{ title }}
+                </h2>
+                <p v-if="enough >= 1">
+                    in stock
+                </p>
+                <p 
+                v-else
+                :class="{soldOut:!enough}"
+                 >
+                    sold out
+                </p>
+                <ul>
+                    <li v-for="detail in details">
+                        {{ detail }}
+                    </li>
+                </ul>
+                <ul>
+                    <li v-for="size in sizes">
+                        {{ size }}
+                    </li>
+                </ul>
+                <div 
+                v-for="(variant,index) in variants" 
+                :key="variant.variantId"
+                :style = "{backgroundColor:variant.variantColor, color:white , width : width + 'px'}"
+                @mouseover="updateImage(index)"
+                >
+                        {{ variant.variantColor }}
+                </div>
+
+                <div class="cart">
+                    <button v-on:click="decrease">
+                        decrease the cart
+                    </button>
+                    <button 
+                    v-on:click="cartCounter" 
+                    :disabled="!enough"
+                    :class="{diasabledbutton:!enough}"
+                    >
+                        add to cart 
+                    </button>
+                    <span>
+                        {{ cart }}
+                    </span>
+                </div>
+
+            </div>
+        </div>
+    `,
+    data(){
+        return {
+            name :'sea food',
+            brand : 'ba',
+            titleLink : 'https://google.com',
+            details : [ 'fish' , 'ocean' , 'fresh'],
+            variants : [
+                {
+                    variantId: 234,
+                    variantColor: 'Red',
+                    variantQuantity : 0,
+                    isOnSale : true ,
+                    variantImage : 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/8d69fcc04fca40aea88c7eaee3b7c367.jpg'
+                },
+                {
+                    variantId: 344,
+                    variantColor: 'blue',
+                    variantQuantity : 10 ,
+                    isOnSale : false ,
+                    variantImage : 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/b44111bc02b641619733c46685033288.jpg'
+    
+                }
+            ],
+            sizes :[ 'medium' , 'large'],
+            white : 'white',
+            width: 50,
+            cart : 0,
+            selectedVariant :0
+        }
     },
     methods:{
         cartCounter(){
@@ -54,4 +115,8 @@ var app = new Vue({
             return this.variants[this.selectedVariant].isOnSale
         }
     }
+})
+
+var app = new Vue({
+    el:'#app',
 })
