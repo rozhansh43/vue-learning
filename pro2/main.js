9446b7c2d570ad799f1e0143f0464ac3a2c701c9
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-04 23:46:27 -0700

    product base added

--- pro2/main.js
+++ pro2/main.js
@@ -1,34 +1,57 @@
 var app = new Vue({
     el:'#app',
     data:{
-        product : 'sea food',
+        name :'sea food',
+        brand : 'ba',
         titleLink : 'https://google.com',
-        enough : 10 ,
-        isOnSale : true ,
         details : [ 'fish' , 'ocean' , 'fresh'],
         variants : [
             {
                 variantId: 234,
                 variantColor: 'Red',
+                variantQuantity : 0,
+                isOnSale : true ,
                 variantImage : 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/8d69fcc04fca40aea88c7eaee3b7c367.jpg'
             },
             {
                 variantId: 344,
                 variantColor: 'blue',
+                variantQuantity : 10 ,
+                isOnSale : false ,
                 variantImage : 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/b44111bc02b641619733c46685033288.jpg'
 
             }
         ],
         sizes :[ 'medium' , 'large'],
+        white : 'white',
+        width: 50,
         cart : 0,
-        image : 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/df0823628fca40419cca88368e00955f.jpg'
+        selectedVariant :0
     },
     methods:{
-        cartCounter : function(){
+        cartCounter(){
             this.cart += 1 
         },
-        updateImage : function(variantImage){
-            this.image = variantImage
+        updateImage(index){
+            this.selectedVariant = index
+            console.log(index)
+        },
+        decrease(){
+            this.cart -= 1
+        }
+    },
+    computed:{
+        title(){
+            return this.name + '' + this.brand
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        enough(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        isOnSale(){
+            return this.variants[this.selectedVariant].isOnSale
         }
     }
 })
