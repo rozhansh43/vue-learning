ccba61e77760f966f851b6d18e4995f4cecb8178
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-06 04:53:39 -0700

    pro8 added

---
+++ pro8/main.js
@@ -0,0 +1,11 @@
+new Vue({
+    el:'#app',
+    data:{
+        show:true,
+        persons:[
+            {name:'Max',age:27},
+            {name:'Sam',age:15},
+            {name:'Ted',age:23}
+        ]
+    }
+})
