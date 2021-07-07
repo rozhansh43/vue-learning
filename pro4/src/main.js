5aba04124b49cc8b92a3c261780aa4fb2e439152
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-07 00:08:24 -0700

    some changes

--- pro4/src/main.js
+++ pro4/src/main.js
@@ -1,14 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import message from "./message.vue";
 import router from "./router";
 import store from "./store";
 
 createApp(App).use(store).use(router).mount("#app");
 
-Vue.component("app-message",message);
-
-new Vue ({
-    el:"#app",
-    render : h =>(App)
-})
