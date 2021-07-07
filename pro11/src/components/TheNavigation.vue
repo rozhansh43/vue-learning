52f493b4208c169d242d20f21cd3649d1657713e
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-07 05:21:53 -0700

    pro11 correct process

--- pro11/src/components/TheNavigation.vue
+++ pro11/src/components/TheNavigation.vue
@@ -1,40 +1,12 @@
 <template>
     <nav id="nav"> 
-        <p>the vue travel app</p>
-       <ul class="nav-links">
-           <li class="links">
-               <router-link to="/">
-               Home
-               </router-link>
-           </li>
-           <li
-            v-for:="destination in destinations" 
-           :key="destination.name"
-           class="links">
-               <router-link 
-               :to="{
-                   name: 'DestinationDetails',
-                   params:{id:destination.id}
-               }">
-               {{ destination.name }}
-               </router-link>
-           </li>
-       </ul>
+        <router-link to="/">Home</router-link>
+        <router-link to="/brazil">Brazil</router-link>
+        <router-link to="/panama">Panama</router-link>
+        <router-link to="/hawaii">Hawaii</router-link>
+        <router-link to="/jamaica">Jamaica</router-link>
     </nav>
 </template>
-
-<script>
-import store from "@/store.js";
-export default{
-    data(){
-        return{
-            destinationId: this.$route.params.id,
-            destinations:store.destinations,
-        }
-    }
-}
-</script>
-
 <style scoped>
 #nav{
     display: flex;
