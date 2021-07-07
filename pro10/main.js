d777e58316d74f628b8ecb976a409311d0315e8f
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-06 22:25:53 -0700

    pro10  added

---
+++ pro10/main.js
@@ -0,0 +1,30 @@
+new Vue({
+    el: '#todo-list-example',
+    data: {
+      newTodoText: '',
+      todos: [
+        {
+          id: 1,
+          title: 'Do the dishes',
+        },
+        {
+          id: 2,
+          title: 'Take out the trash',
+        },
+        {
+          id: 3,
+          title: 'Mow the lawn'
+        }
+      ],
+      nextTodoId: 4
+    },
+    methods: {
+      addNewTodo: function () {
+        this.todos.push({
+          id: this.nextTodoId++,
+          title: this.newTodoText
+        })
+        this.newTodoText = ''
+      }
+    }
+  })
