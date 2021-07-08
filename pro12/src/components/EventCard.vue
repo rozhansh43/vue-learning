a12d88e29699db8068f71447584eee17a0452fbe
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-08 01:41:18 -0700

    card added

--- pro12/src/components/EventCard.vue
+++ pro12/src/components/EventCard.vue
@@ -1,19 +1,54 @@
 <template>
-    <div>
-        
-    </div>
+  <div class="event-card -shadow ">
+      <span>
+          @{{ events.time }} on {{ events.date }}
+      </span>
+    <h4 class="title">
+      {{ events.title }}
+    </h4>
+    <span>
+        {{ events.attendees.length }} attending
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            quote:"text message"
-        }
-    }
+  data() {
+    return {
+      events: {
+        id: 1,
+        title: "park cleanup",
+        date: "tues aug 19 , 2018 ",
+        time: "5:00",
+        attendees: [
+            { id : 'abc123' , name : 'Adam jahr'},
+            { id : 'edrf89' , name : 'gregg poli'},
+
+        ],
+      },
+    };
+  },
 };
 </script>
 
 <style scoped>
-
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
 </style>
--- pro12/src/views/EventList.vue
+++ pro12/src/views/EventList.vue
@@ -4,14 +4,19 @@
             Event Listing
         </h1>
 
-        <router-link :to="{ name : 'event-show' , params:{ id:1 }}">Show Event #1</router-link> |
+        <EventCard />
+
+        <router-link :to="{ name : 'event-show' , params:{ id:1 }}">Show Event #1</router-link> 
 
     </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue';
 export default {
-    
+    components : {
+        EventCard
+    }
 }
 </script>
 
