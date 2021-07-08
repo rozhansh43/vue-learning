b76d179ec46bda3033cb390cb2a151b96c07392e
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-08 03:27:55 -0700

    pro12 mediabox should be completed

---
+++ pro12/src/components/MetaBox.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <UserAvatar/>
+    <slot name="heading"></slot>
+    <slot name="paragraph"></slot>
+  </div>
+</template>
--- pro12/src/views/EventList.vue
+++ pro12/src/views/EventList.vue
@@ -1,24 +1,33 @@
 <template>
-    <div>
-        <h1>
-            Event Listing
-        </h1>
+  <div>
+    <h1>Event Listing</h1>
 
-        <EventCard />
-        <BaseIcon/>
+    <EventCard />
+    <BaseIcon />
 
-    </div>
+    <MediaBox>
+      <h2 slot="heading">
+          Adam j
+        </h2>
+      <template>
+        <p slot="paragraph">
+            My words
+        </p>
+        <BaseIcon name="book" />
+      </template>
+    </MediaBox>
+  </div>
 </template>
 
 <script>
-import EventCard from '@/components/EventCard.vue';
+import EventCard from "@/components/EventCard.vue";
+import BaseIcon from '../components/BaseIcon.vue';
 export default {
-    components : {
-        EventCard
-    }
-}
+  components: {
+    EventCard,
+    BaseIcon,
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
