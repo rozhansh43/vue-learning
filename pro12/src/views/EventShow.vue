36caa98bb857e2b35c5741d90b2d12b53b6a62ee
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-10 01:13:28 +0430

    real world vue done

--- pro12/src/views/EventShow.vue
+++ pro12/src/views/EventShow.vue
@@ -18,12 +18,22 @@
       </h5>
     </div>
 
-    <BaseIcon name="map"><h2>Location</h2></BaseIcon>
+    <BaseIcon name="map">
+     <h2>
+         Location
+     </h2>
+    </BaseIcon>
 
-    <address>{{ event.location }}</address>
+    <address>
+        {{ event.location }}
+    </address>
 
-    <h2>Event details</h2>
-    <p>{{ event.description }}</p>
+    <h2>
+        Event details
+    </h2>
+    <p>
+        {{ event.description }}
+    </p>
 
     <h2>
       Attendees
@@ -31,6 +41,7 @@
           {{ event.attendees ? event.attendees.length : 0 }}
       </span>
     </h2>
+    
     <ul class="list-group">
       <li
         v-for="(attendee, index) in event.attendees"
