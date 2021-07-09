ab9632ed5d98734708caccbc7d3de14d379bbf94
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-10 04:05:14 +0430

    brand added

--- pro13/main.js
+++ pro13/main.js
@@ -1,21 +1,39 @@
+Vue.config.devtools = true;
+
 var app = new Vue({
     el: '#app',
     data: {
         product: 'socks',
-        image: {
-            src: './assets/images/socks_green.jpg',
-            name: 'socks'
-        },
+        name: 'socks',
+        brand: 'Vue',
         inventory: 2,
+        inStock: true,
+        image: './assets/images/socks_green.jpg',
         details: ['cotton', 'polyester', 'gender-nuteral'],
         varients: [{
                 varientId: 3435,
-                varientColor: 'green'
+                varientColor: 'green',
+                varientImage: './assets/images/socks_green.jpg',
             },
             {
                 varientId: 4343,
-                varientColor: 'blue'
+                varientColor: 'blue',
+                varientImage: './assets/images/socks_blue.jpg',
             }
-        ]
+        ],
+        cart: 0,
+    },
+    methods: {
+        AddToCart() {
+            this.cart += 1
+        },
+        updateProduct(varientImage) {
+            this.image = varientImage
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        }
     }
 })
