3b10fe532ae5c7b50cffe828c1bf5036a9c8bafd
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-10 01:18:34 -0700

    pro 13 check the review tab

--- pro13/main.js
+++ pro13/main.js
@@ -1,3 +1,5 @@
+var eventBus = new Vue()
+
 Vue.config.devtools = true;
 
 Vue.component ('product', {
@@ -47,41 +49,19 @@ Vue.component ('product', {
                     Add to cart
                 </button>
 
+         
+
             </div>
 
-            <div>
-                <h2>
-                    Reviews 
-                </h2>
+            <product-tabs :reviews="reviews">
 
-                <p v-if="!reviews.length">
-                    There are no reviews yet
-                </p>
-
-                <ul>
-                    <li v-for="review in reviews">
-                        <p>
-                        {{ review.name }}
-                        </p>
-
-                        <p>
-                        {{ review.rating }}
-                        </p>
-
-                        <p>
-                        {{ review.review }}
-                        </p>
-                    </li>
-                </ul>
-            </div>            
-
-            <product-review @review-submitted="addReview"></product-review>
+            </product-tabs>
 
         </div>
     `,
     data() {
         return{
-            product: 'socks',
+        product: 'socks',
         name: 'socks',
         brand: 'Vue',        
         selectedVariant: 0,
@@ -110,9 +90,6 @@ Vue.component ('product', {
             this.selectedVariant = index
             console.log(index)
         },
-        addReview(productReview) {
-            this.reviews.push(productReview)
-        }
 
     },
     computed: {
@@ -130,6 +107,11 @@ Vue.component ('product', {
                 return 'Free'
             }
             return 3.99
+        },
+        mounted() {
+            eventBus.$on('review-submitted', productReview => {
+                this.reviews.push(productReview)
+            })
         }
     }
 })
@@ -210,7 +192,7 @@ Vue.component('product-review', {
                     review: this.review,
                     rating: this.rating
                 }
-                this.$emit('review-submitted', productReview)
+                eventBus.$emit('review-submitted', productReview)
                 this.name = null,
                 this.review = null,
                 this.rating = null
@@ -221,6 +203,65 @@ Vue.component('product-review', {
             }
         }
     }
+})
+
+Vue.component('product-tabs', {
+    props: {
+        reviews: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+    <div>
+        <span class="tab" 
+        :class="{ activeTab : selectedTab === tab}"
+        v-for="(tab, index) in tabs" 
+        :key="index"
+        @click="selectedTab = tab"
+        >
+            {{ tab }}
+        </span>
+
+        <div v-show="selectedTab === 'Review'">
+            <h2>
+                Reviews 
+            </h2>
+
+            <p v-if="!reviews.length">
+                There are no reviews yet
+            </p>
+
+            <ul v-else>
+                <li v-for="review in reviews">
+                    <p>
+                    {{ review.name }}
+                    </p>
+
+                    <p>
+                    {{ review.rating }}
+                    </p>
+
+                    <p>
+                    {{ review.review }}
+                    </p>
+                </li>
+            </ul>
+    </div>            
+
+    <product-review 
+    v-show="selectedTab === 'Make a Review'"
+    >
+    </product-review>
+
+    </div>
+    `,
+    data() {
+        return {
+            tabs: ['Reviews', 'Make a Review'],
+            selectedTab: "Reviews"
+        }
+    },
 })
 
 var app = new Vue({
