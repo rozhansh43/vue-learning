53b0590a25782dad48fabcdd9dbec3bc661b0dd4
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-11 07:49:35 +0430

    array plans added

--- vueSchool-planPicker/main.js
+++ vueSchool-planPicker/main.js
@@ -3,12 +3,21 @@ Vue.config.devtools = true
 Vue.component('plan', {
     template: '#plan-template',
 
-    props: ['name']
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+    }
 })
 
 var App = new Vue({
     el: '#app',
     data: {
-        plans: ['The Addict', 'The Curious', 'The Hacker']
+        plans: ['The Addict', 'The Curious', 'The Hacker'],
+        image: {
+            title: 'coffee',
+            src: 'coffee.jpg'
+        }
     }
 })
